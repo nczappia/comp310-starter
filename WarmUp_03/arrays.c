db02f9b188b7930d8958c178e720d88d996e6866
Nick Zappia <nczappia15@gmail.com>
2024-01-21 03:21:45 +0000

    added arrays

---
+++ WarmUp_03/arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int numbers[5] = {1, 2, 3, 4, 5};
+	int sum = 0;
+	int length = sizeof(numbers) / sizeof(numbers[0]);
+
+	for(int i=0; i<length; ++i){
+		sum += numbers[i];
+	}
+	double average = sum / length;
+
+	printf("Sum: %d\n", sum);
+	printf("Average: %f\n", average);
+}
+
