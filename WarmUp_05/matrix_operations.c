a4525a5a7f7ed729a3514e454f3a62f241f3649a
Nick Zappia <nczappia15@gmail.com>
2024-01-22 05:05:46 +0000

    minor changes to matricies

--- WarmUp_05/matrix_operations.c
+++ WarmUp_05/matrix_operations.c
@@ -2,7 +2,6 @@
 
 void printMatrix(int matrix[2][2], int rows, int columns)
 {
-    printf("*Printing Matrix*\n");
     for(int i=0; i<rows; ++i){
 	for(int j=0; j<columns; ++j){
 	    printf("%d\t", matrix[i][j]);
@@ -20,13 +19,12 @@ void addMatricies(int matrix1[2][2], int matrix2[2][2], int sum[2][2],  int rows
     }
 }
 
-void multiplyMatricies(int matrix1[2][2], int matrix2[2][2], int product[2][2], int matrix1_rows, int matrix1_columns, int matrix2_rows, int matrix2_columns)
+void multiplyMatricies(int matrix1[2][2], int matrix2[2][2], int product[2][2], int matrix1_rows, int matrix2_rows, int matrix2_columns)
 {
     for(int i=0; i<matrix1_rows; ++i){
 	for(int j=0; j<matrix2_columns; ++j){
 	    for(int k=0; k<matrix2_rows; ++k){
 		product[i][j] += matrix1[i][k] * matrix2[k][j];
-		printf("Row: %d\tColumn: %d\tMatrix1: %d\tMatrix2: %d\tProduct: %d\tStep: %d\t\n", i, j, matrix1[i][k], matrix2[k][j], product[i][j], k);
 	    }
 	}
     }
@@ -39,18 +37,15 @@ int main()
 
     int matrix1_rows = 2, matrix1_columns = 2, matrix2_rows = 2, matrix2_columns = 2;
     
-    int sum[2][2];
-    int product[2][2];
-    
-    //printMatrix(matrix1, matrix1_rows, matrix1_columns);
-    //printMatrix(matrix2, matrix2_rows, matrix2_columns);
-    
-    //addMatricies(matrix1, matrix2, sum, matrix1_rows, matrix1_columns);
-    //printMatrix(sum, matrix1_rows, matrix1_columns);
-    
-    printMatrix(product, matrix1_columns, matrix2_rows);
-    multiplyMatricies(matrix1, matrix2, product, matrix1_rows, matrix1_columns, matrix2_rows, matrix2_columns);
-    printMatrix(product, matrix1_columns, matrix2_rows);
+    int sum[2][2] = {{0}};
+    int product[2][2] = {{0}};
+   
+    addMatricies(matrix1, matrix2, sum, matrix1_rows, matrix1_columns);
+    multiplyMatricies(matrix1, matrix2, product, matrix1_rows, matrix2_rows, matrix2_columns); 
+    printf("Matrix Addition:\n");
+    printMatrix(sum, matrix1_rows, matrix1_columns);
+    printf("Matrix Multiplication:\n");
+    printMatrix(product, matrix2_rows, matrix1_columns);
 
     return 0;
 }
