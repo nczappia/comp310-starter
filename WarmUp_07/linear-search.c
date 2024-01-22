ca0bd87a48f39c672676cbb1bda484b8d19365ef
Nick Zappia <nczappia15@gmail.com>
2024-01-22 05:04:34 +0000

    linear search added

---
+++ WarmUp_07/linear-search.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int arr_length, int target)
+{
+    for(int i=0; i<arr_length; ++i){
+	if(arr[i] == target)
+	    return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int array[] = {1, 4, 7, 9, 12};
+    int target = 9;
+    int arr_length = sizeof(array)/sizeof(array[0]);
+    int index = linearSearch(array, arr_length, target);
+    if(index != -1)
+	printf("Element found at index: %d\n", index);
+    else
+	printf("Element not found\n");
+}
