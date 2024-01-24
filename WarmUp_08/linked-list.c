72197eaeb0d21c336ccb30ad26fe92dd832a223b
Nick Zappia <nczappia15@gmail.com>
2024-01-24 17:00:26 +0000

    added linked list for debugging

--- WarmUp_08/linked-list.c
+++ WarmUp_08/linked-list.c
@@ -2,7 +2,31 @@
 
 #include "linked-list.h"
 
+void push(int new_data)
+{
+
+}
+
+void deleteNode(int key)
+{
+
+}
+
+void printList(struct LinkedList *llist)
+{
+    struct Node *tnode = llist->head;
+   
+    while(tnode->next != NULL)
+    {
+        printf("%d ", tnode->data);
+        tnode = tnode->next;
+    }	
+}
+
+
 int main()
 {
-    
+    struct LinkedList llist = {NULL};
+
+    printList(&llist);    
 }
