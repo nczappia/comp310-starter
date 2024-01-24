5451da07913a72e788f10a62abbdbc2c449c89b1
Nick Zappia <nczappia15@gmail.com>
2024-01-24 23:23:57 +0000

    linked list debugging

--- WarmUp_08/linked-list.c
+++ WarmUp_08/linked-list.c
@@ -18,12 +18,14 @@ void push(struct LinkedList *llist, int new_data)
 
     //Change the assigned head node in the linked list to the new node
     llist->head = new_node; 
-    //printf("Node Address: %p\n", (void*)new_node);
+    printf("Node Address: %p\n", (void*)new_node);
+    printf("Made through\n");
 }
 
 void deleteNode(struct LinkedList *llist, int key)
 {
     //Begin at head node
+    printf("Deleting...\n");
     struct Node *temp = llist->head, *prev = NULL;
     printf("Temp Address: %p\tPrevious Address: %p\n", (void*)temp, (void*)prev);
     //Check if head node contains matching value
@@ -31,29 +33,38 @@ void deleteNode(struct LinkedList *llist, int key)
         llist->head = temp->next;
     }
     //Iterate through list until matching value or end of list
-    int count = 0;    
-    while(temp != NULL && temp->data != key){
-	prev = temp;
+    int count = 1;
+    while(temp != NULL){
+	if(temp->data == key){
+	    printf("Found\n");
+	    break;
+	}
+        printf("Temp: %p\tData: %d\tKey: %d\t\n", temp, temp->data, key);
+ 	prev = temp;
 	temp = temp->next;
-        printf("Temp Address: %p\tPrevious Address: %p\n", (void*)temp, (void*)prev);
-        count++;
+        printf("Iteration %d:\n", count);
+     	printf("Temp Address: %p\tPrevious Address: %p\n", (void*)temp, (void*)prev);
+	printf("Finished Iteration %d\n", count);
+        count++;	
+        printf("Up next...\nTemp: %p\tData: %d\tKey: %d\t\n", temp, temp->data, key);
     }
+    printf("Found 8\n");
     //If end of list found, do nothing
     if(temp == NULL){
         return;
     }
+    printf("Through NULL check\n");
     //If matching value found, delete the node and reassign next addresses
     prev->next = temp->next;
-    free(&temp->data);
-    free(temp->next);
+    printf("8 removed\n");
     free(temp);
 }
 
 void printList(struct LinkedList *llist)
 {
+    printf("IN"); 
     struct Node *tnode = llist->head;
-    printf("yo"); 
-    while(tnode != NULL)
+    while(tnode->next != NULL)
     {
         printf("%d ", tnode->data);
         tnode = tnode->next;
@@ -66,13 +77,14 @@ int main()
 {
     struct LinkedList llist;
     
+    printf("Starting...\n");
     push(&llist, 5); 
     push(&llist, 8);
     push(&llist, 10);
-    printf("Made it");    
+    printf("Made it\n");    
     printList(&llist); 
 
     deleteNode(&llist, 8);
     
-    printList(&llist);   
+    //printList(&llist);   
 }
